1e50008b873bde224a86011330d9231364ca2b61
pavan1419 <ptechhat@gmail.com>
2023-08-12 22:04:28 +0530

    ok ok

--- part2/dom-tutor/111.js
+++ part2/dom-tutor/111.js
@@ -1,34 +1,52 @@
-const rootNode = document.getRootNode()
-const htmlElementNode = rootNode.childNodes[0]
+// const rootNode = document.getRootNode()
+// const htmlElementNode = rootNode.childNodes[0]
 
-// console.log(rootNode.childNodes) 
-// console.log(htmlElementNode.childNodes)
+// // console.log(rootNode.childNodes) 
+// // console.log(htmlElementNode.childNodes)
 
-// child relation 
+// // child relation 
 
-const head = htmlElementNode.childNodes[0]
-const text = htmlElementNode.childNodes[1]
-const body = htmlElementNode.childNodes[2]
-
-
-
-console.log(head)
-console.log(body)
-
-// parent relation 
-
-console.log(text.parentElement)
+// const head = htmlElementNode.childNodes[0]
+// const text = htmlElementNode.childNodes[1]
+// const body = htmlElementNode.childNodes[2]
 
 
-// siblineg releation
 
-console.log(head.nextSibling)
+// // console.log(head)
+// // console.log(body)
+
+// // parent relation 
+
+// // console.log(text.parentElement)
 
 
-// ---------------------------------------
-const h1 = document.querySelector("h1")
-let div  = h1.parentElement
+// // siblineg releation
 
-div.style.color = "#333"
+// console.log(head.nextSibling)
 
-div.style.backgroundColor = "#333"
+
+// // ---------------------------------------
+// const h1 = document.querySelector("h1")
+// const body1  = h1.parentElement.parentNode
+// console.log(body1)
+
+// body1.style.color = "#efefef"
+
+// body1.style.backgroundColor = "#333"
+
+
+// const body = document.body
+// console.log(body)
+
+// body.style.color = "red"
+
+// const head = document.querySelector("head")
+// // console.log(head)
+// const title = document.querySelector("title")
+// const text  = title.childNodes
+// console.log(text)
+
+const container = document.querySelector(".container")
+console.log(container.children)
+
+// container.style.backgroundColor = "red"

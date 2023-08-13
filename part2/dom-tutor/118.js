7acefe16ee1019aaf547424a7c399b8623cd6e53
pavan1419 <ptechhat@gmail.com>
2023-08-13 07:03:35 +0530

    info

--- part2/dom-tutor/118.js
+++ part2/dom-tutor/118.js
@@ -1,10 +1,21 @@
 // static list vs live list
 
+// querySelector gives us static list 
+
+// const listItems = document.querySelectorAll(".todo-list li")
+// const newLi = document.createElement("li")
+// newLi.textContent = "item item "
+// const ul = document.querySelector(".todo-list")
+// ul.append(newLi)
+// console.log(listItems)
 
 
-const listItems = document.querySelectorAll(".todo-list li")
+
+// getElementbySOmthing gives us live list 
+
+const ul = document.querySelector(".todo-list")
+const listItems1 = ul.getElementsByTagName("li")
 const newLi = document.createElement("li")
 newLi.textContent = "item item "
-const ul = document.querySelector(".todo-list")
 ul.append(newLi)
-console.log(listItems)
+console.log(listItems1)
---
+++ part2/dom-tutor/119.js
@@ -0,0 +1,6 @@
+// how to get the dimension of any elemensts 
+
+const sectionTodo =  document.querySelector(".todo-section")
+const info = sectionTodo.getBoundingClientRect()
+
+console.log(info)

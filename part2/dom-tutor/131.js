bab2b0f669dd61ab1a7b611ce3a132b0096e4e5d
pavan1419 <ptechhat@gmail.com>
2023-08-15 15:06:22 +0530

    color changer

--- part2/dom-tutor/131.js
+++ part2/dom-tutor/131.js
@@ -1,9 +1,30 @@
-// setInterval
-console.log("script started")
+// // setInterval
+// console.log("script started")
 
-// setInterval("iii", 2000) 
-// setInterval(()=>{
-//     console.log(Math.random()*100)
-// },2000)
+// // setInterval("iii", 2000) 
+// // setInterval(()=>{
+// //     console.log(Math.random()*100)
+// // },2000)
 
-console.log("scritp end ")
+// console.log("scritp end ")
+
+const Body = document.body
+const button = document.querySelector("button")
+// console.log(button)
+
+const intervalID = setInterval(()=>{
+    const red = Math.floor(Math.random()*126)
+    const green = Math.floor(Math.random()*126)
+    const blue = Math.floor(Math.random()*126)
+    const rgb = `rgb(${red}, ${green}, ${blue})`
+    // console.log(rgb)
+    Body.style.background = rgb;
+}, 1000)
+
+button.addEventListener("click",()=>{
+    clearInterval(intervalID)
+    button.textContent = Body.style.background
+})
+
+
+console.log(intervalID)

b22099f06773d11d21fa1e6b677bc6b89f681d2c
pavan1419 <ptechhat@gmail.com>
2023-08-17 07:40:40 +0530

    ajax sstart

---
+++ part2/dom-tutor/139.js
@@ -0,0 +1,9 @@
+// AJAX
+
+// ajax : Asynchronous Javascript and XML
+
+// is a set of web development techniques
+// using many technologies on the clint side 
+// to create asynchronous web applications. 
+
+// with ajax web application can send and retrive 
